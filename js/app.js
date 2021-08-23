95025021eca81b647d7c74d4f55b3fb2e3bbf640
epix360 <porter.eric.e@gmail.com>
2021-08-23 16:16:09 -0600

    Parse JSON

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const userAction = async () => {
     const response = await fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=00bc5f819bb0f261669e2bc249379ec3');
     const myJson = await response.json(); //extract JSON from the http response
-    console.log(myJson);
+    const obj = JSON.parse(myJson);
+    console.log('obj.name');
+    console.log('obj.description');
   }
