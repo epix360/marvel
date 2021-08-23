bbc1537d0ee6da69288e1580672bf0b0dcfd72c7
epix360 <porter.eric.e@gmail.com>
2021-08-23 16:22:01 -0600

    stringify json

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const userAction = async () => {
     const response = await fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=00bc5f819bb0f261669e2bc249379ec3');
     const myJson = await response.json(); //extract JSON from the http response
-    const obj = JSON.parse(myJson);
-    console.log('obj.name');
-    console.log('obj.description');
+    const obj = JSON.parse(JSON.stringify(myJson));
+    console.log(obj.name);
+    console.log(obj.description);
   }
