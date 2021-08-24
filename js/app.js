76690815ea950d191da27cdd957acad8350eee2a
epix360 <porter.eric.e@gmail.com>
2021-08-24 10:22:58 -0600

    configured request url

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
+const baseUrl = 'https://gateway.marvel.com:443/v1/public/characters';
+const apiKey = '00bc5f819bb0f261669e2bc249379ec3';
+const privateKey = '668ac08feb3780cde547a8f3202e99959d02329d';
+const timeStamp = Date.now() / 1000 | 0;
+const hash = md5(timeStamp+privateKey+apiKey);
+const completeUrl = baseUrl + '?ts=' + timeStamp + '&apikey=' + apiKey + '&hash=' + hash;
+
 const userAction = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const response = await fetch(completeUrl);
     const myJson = await response.json(); //extract JSON from the http response
     const obj = JSON.parse(JSON.stringify(myJson));
-    console.log(obj.title);
-    console.log(obj.completed);
+    console.log(obj.data.results.name);
+    console.log(obj.data.results.description);
   }
 
 const generateHeroBtn = document.getElementById('generateHeroBtn').addEventListener('click', userAction)
-
